5135c7804feeecc94082cb48dc4523b39bef28d6
PratyushSinghal20 <pratyush1820@gmail.com>
2025-06-14 08:37:24 +0530

    Update numberguess_cpp.py

--- numberguess_cpp.py
+++ numberguess_cpp.py
@@ -1,31 +1,44 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
 
 int main() {
-    int number, guess, tries = 0;
+    int number, guess, attempts;
+    char playAgain;
 
-    // Initialize random number generator
+    // Seed the random number generator
     srand(time(0));
-    number = rand() % 100 + 1;  // Random number between 1 and 100
 
-    printf("Welcome to the Number Guessing Game!\n");
-    printf("I'm thinking of a number between 1 and 100.\n");
-
-    // Loop until correct guess
     do {
-        printf("Enter your guess: ");
-        scanf("%d", &guess);
-        tries++;
+        number = rand() % 100 + 1; // random number between 1 and 100
+        attempts = 0;
 
-        if (guess > number)
-            printf("Too high! Try again.\n");
-        else if (guess < number)
-            printf("Too low! Try again.\n");
-        else
-            printf("Correct! You got it in %d tries.\n", tries);
+        cout << "Welcome to the Number Guessing Game!" << endl;
+        cout << "I have selected a number between 1 and 100." << endl;
 
-    } while (guess != number);
+        do {
+            cout << "Enter your guess: ";
+            cin >> guess;
+            attempts++;
+
+            if (guess > number) {
+                cout << "Too high! Try again." << endl;
+            } else if (guess < number) {
+                cout << "Too low! Try again." << endl;
+            } else {
+                cout << "Congratulations! You guessed the number in " << attempts << " attempts." << endl;
+            }
+        } while (guess != number);
+
+        cout << "Do you want to play again? (y/n): ";
+        cin >> playAgain;
+
+    } while (playAgain == 'y' || playAgain == 'Y');
+
+    cout << "Thank you for playing!" << endl;
 
     return 0;
 }
+
